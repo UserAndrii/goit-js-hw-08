41bf04bc055b14d1161fad4250667f792667ae74
Andrii Gadar <gadar.andre@gmail.com>
2023-03-07 20:17:40 +0200

    task-01 is completed

---
+++ src/js/01-gallery.js
@@ -0,0 +1,37 @@
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+galleryContainer.insertAdjacentHTML(
+  'beforeend',
+  createGalleryItemsMarkup(galleryItems)
+);
+
+function createGalleryItemsMarkup(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `
+            <a class="gallery__item" href="${original}" rel="noreferrer noopener">
+               <img class="gallery__image"
+                    src="${preview}" 
+                    alt="${description}" 
+                    loading="lazy"
+                    width="720"
+                    height="480"
+                />
+            </a>
+        `;
+    })
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  fadeSpeed: 250,
+});
