ad2742553ec6228574afbc8b5fdbcbaa95c5fd18
Andrii Gadar <gadar.andre@gmail.com>
2023-03-08 16:57:06 +0200

    task-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+const formEl = document.querySelector('.feedback-form');
+const formData = {};
+
+formEl.addEventListener('submit', onFormSubmit);
+formEl.addEventListener('input', throttle(onFormInput, 500));
+
+populateInput();
+
+function onFormInput(e) {
+    formData[e.target.name] = e.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  console.log(formData);
+
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function populateInput() {
+  const isFilledInput = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (isFilledInput) {
+    for (const key of Object.keys(isFilledInput)) {
+      formEl.elements[key].value = isFilledInput[key];
+    }
+  }
+}
