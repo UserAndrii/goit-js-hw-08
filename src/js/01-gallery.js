f1974bbfdf593b0aa999d6f11f6d40961a97e74b
Andrii Gadar <gadar.andre@gmail.com>
2023-03-08 12:38:54 +0200

    task-02 is completed

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,8 +4,6 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
-
 const galleryContainer = document.querySelector('.gallery');
 galleryContainer.insertAdjacentHTML(
   'beforeend',
